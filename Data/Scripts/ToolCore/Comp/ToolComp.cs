50618943c691abf5e138497cecdff2ff50afa59f
BDCarrillo <90526940+BDCarrillo@users.noreply.github.com>
2024-11-04 14:43:07 -0600

    Fix for tracktargets cycling off not updating AV

--- Data/Scripts/ToolCore/Comp/ToolComp.cs
+++ Data/Scripts/ToolCore/Comp/ToolComp.cs
@@ -91,7 +91,25 @@ namespace ToolCore.Comp
         internal bool LastPushSucceeded = true;
 
         internal bool Draw;
-        internal bool TrackTargets;
+        internal bool _trackTargets;
+        internal bool TrackTargets
+        {
+            get { return _trackTargets; }
+            set
+            {
+                if (_trackTargets == value)
+                    return;
+
+                _trackTargets = value;
+
+                UpdateAvState(Trigger.Activated, value);
+                if (!value)
+                {
+                    //WasHitting = false;
+                    UpdateHitInfo(false);
+                }
+            }
+        }
         internal bool UseWorkColour;
 
         internal bool Working;
